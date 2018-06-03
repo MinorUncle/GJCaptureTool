4208da305a0a42baaff5ccac604b7e2d80a0321d
MirrorUncle <1065517719@qq.com>
2018-06-03 20:14:16 +0800

    clang-format

--- GJLiveEngine/CrossPlatform/GJLog.c
+++ GJLiveEngine/CrossPlatform/GJLog.c
@@ -15,31 +15,30 @@
 #include "GJList.h"
 #include "GJBufferPool.h"
 
-#define MAX_PRINT_LEN	2048
+#define MAX_PRINT_LEN 2048
 
-GJClass* defaultDebug = (GJClass*)&Default_WARNING;
+GJClass *defaultDebug = (GJClass *) &Default_WARNING;
 
-static FILE *fmsg;
+static FILE *         fmsg;
 static GJ_LogCallback gj_log_default, *cb = gj_log_default;
 
-static const char *levels[] = {"GJ_NONE","GJ_FORBID","GJ_ERROR", "GJ_WARNING", "GJ_DEBUG","GJ_INFO","GJ_ALL"};
-const GJClass*  debugClass[] = {&Default_NONE,&Default_FORBID,&Default_ERROR,&Default_WARNING,&Default_DEBUG,&Default_INFO,&Default_ALL};
-inline static GVoid gj_log_default(GJClass* dClass,GJ_LogLevel level ,const char *pre, const char *format, va_list vl)
-{
+static const char * levels[]     = {"GJ_NONE", "GJ_FORBID", "GJ_ERROR", "GJ_WARNING", "GJ_DEBUG", "GJ_INFO", "GJ_ALL"};
+const GJClass *     debugClass[] = {&Default_NONE, &Default_FORBID, &Default_ERROR, &Default_WARNING, &Default_DEBUG, &Default_INFO, &Default_ALL};
+inline static GVoid gj_log_default(GJClass *dClass, GJ_LogLevel level, const char *pre, const char *format, va_list vl) {
 #ifdef GJ_DEBUG
     GJAssert(dClass != GNULL, "dClass不能为null,在本文件转换");
-    char str[MAX_PRINT_LEN]="";
-    vsnprintf(str, MAX_PRINT_LEN-1, format, vl);
-    if ( !fmsg ) fmsg = stderr;
-    
-    struct tm *local;
+    char str[MAX_PRINT_LEN] = "";
+    vsnprintf(str, MAX_PRINT_LEN - 1, format, vl);
+    if (!fmsg) fmsg = stderr;
+
+    struct tm *    local;
     struct timeval t;
-    gettimeofday(&t,NULL);
-    local=localtime(&t.tv_sec);
+    gettimeofday(&t, NULL);
+    local = localtime(&t.tv_sec);
     if (dClass->className != GNULL) {
-        fprintf(fmsg, "[%02d:%02d:%02d:%03d][%s][%s][%s]:%s\n",local->tm_hour,local->tm_min,local->tm_sec,t.tv_usec/1000,levels[level],dClass->className,pre, str);
-    }else{
-        fprintf(fmsg, "[%02d:%02d:%02d:%03d][%s][%s]:%s\n",local->tm_hour,local->tm_min,local->tm_sec,t.tv_usec/1000,levels[level],pre, str);
+        fprintf(fmsg, "[%02d:%02d:%02d:%03d][%s][%s][%s]:%s\n", local->tm_hour, local->tm_min, local->tm_sec, t.tv_usec / 1000, levels[level], dClass->className, pre, str);
+    } else {
+        fprintf(fmsg, "[%02d:%02d:%02d:%03d][%s][%s]:%s\n", local->tm_hour, local->tm_min, local->tm_sec, t.tv_usec / 1000, levels[level], pre, str);
     }
     fflush(fmsg);
 
@@ -49,152 +48,143 @@ inline static GVoid gj_log_default(GJClass* dClass,GJ_LogLevel level ,const char
 #endif
 }
 
+static pthread_t     _logRunloop;
+static GJList *      _messageList;
+static GJBufferPool *_logBufferPool;
+typedef struct _LogMessage {
 
+} GJ_LogMessage;
 
-static pthread_t _logRunloop;
-static GJList* _messageList;
-static GJBufferPool* _logBufferPool;
-typedef struct _LogMessage{
-    
-}GJ_LogMessage;
-
-static GVoid gj_log_runloop(GJClass* dClass,GJ_LogLevel level ,const char *pre, const char *format, va_list vl){
+static GVoid gj_log_runloop(GJClass *dClass, GJ_LogLevel level, const char *pre, const char *format, va_list vl) {
 #ifdef GJ_DEBUG
     GJAssert(dClass != GNULL, "dClass不能为null,在本文件转换");
     //    GJBufferPoolGetSizeData(x, <#y#>)
-    char str[MAX_PRINT_LEN]="";
-    vsnprintf(str, MAX_PRINT_LEN-1, format, vl);
-    if ( !fmsg ) fmsg = stderr;
-    
-    struct tm *local;
+    char str[MAX_PRINT_LEN] = "";
+    vsnprintf(str, MAX_PRINT_LEN - 1, format, vl);
+    if (!fmsg) fmsg = stderr;
+
+    struct tm *    local;
     struct timeval t;
-    gettimeofday(&t,NULL);
-    local=localtime(&t.tv_sec);
-    
-    GChar* strData = GNULL;
+    gettimeofday(&t, NULL);
+    local = localtime(&t.tv_sec);
+
+    GChar *strData = GNULL;
     if (dClass->className != GNULL) {
         GLong strLen = strlen(pre) + strlen(str) + strlen(levels[level]) + strlen(dClass->className) + 23;
-        strData = (GChar*)GJBufferPoolGetSizeData(_logBufferPool, (GInt32)strLen);
-        sprintf(strData, "[%02d:%02d:%02d:%03d][%s][%s][%s]:%s\n",local->tm_hour,local->tm_min,local->tm_sec,t.tv_usec/1000,levels[level],dClass->className,pre, str);
-    }else{
+        strData      = (GChar *) GJBufferPoolGetSizeData(_logBufferPool, (GInt32) strLen);
+        sprintf(strData, "[%02d:%02d:%02d:%03d][%s][%s][%s]:%s\n", local->tm_hour, local->tm_min, local->tm_sec, t.tv_usec / 1000, levels[level], dClass->className, pre, str);
+    } else {
         GLong strLen = strlen(pre) + strlen(str) + strlen(levels[level]) + 23;
-        strData = (GChar*)GJBufferPoolGetSizeData(_logBufferPool, (GInt32)strLen);
-        sprintf(strData, "[%02d:%02d:%02d:%03d][%s][%s]:%s\n",local->tm_hour,local->tm_min,local->tm_sec,t.tv_usec/1000,levels[level],pre, str);
+        strData      = (GChar *) GJBufferPoolGetSizeData(_logBufferPool, (GInt32) strLen);
+        sprintf(strData, "[%02d:%02d:%02d:%03d][%s][%s]:%s\n", local->tm_hour, local->tm_min, local->tm_sec, t.tv_usec / 1000, levels[level], pre, str);
     }
     listPush(_messageList, strData);
     if (level == GJ_LOGFORBID && dClass->dLevel >= GJ_LOGDEBUG) {
         assert(0);
     }
 #endif
-    
 }
-static void * logRunloop(void * userData){
+static void *logRunloop(void *userData) {
     pthread_setname_np("Loop.GJLog");
     while (fmsg) {
-        GChar* message = GNULL;
-        if (listPop(_messageList, (GHandle*)(&message), GINT32_MAX)) {
+        GChar *message = GNULL;
+        if (listPop(_messageList, (GHandle *) (&message), GINT32_MAX)) {
             fprintf(fmsg, "%s", message);
 #ifdef DEBUG
             fprintf(stdout, "%s", message);
 #endif
-            GJBufferPoolSetData(_logBufferPool, (GUInt8*)message);
+            GJBufferPoolSetData(_logBufferPool, (GUInt8 *) message);
         }
     }
     return GNULL;
 }
 
-GVoid GJ_LogSetOutput(char *file)
-{
-    
+GVoid GJ_LogSetOutput(char *file) {
+
     if (fmsg != NULL && fmsg != stderr) {
         fclose(fmsg);
         fmsg = NULL;
         listEnablePop(_messageList, GFalse);
         pthread_join(_logRunloop, GNULL);
         _logRunloop = GNULL;
-        cb = gj_log_default;
+        cb          = gj_log_default;
         if (_messageList) {
             listFree(&_messageList);
         }
         if (_logBufferPool) {
             GJBufferPoolClean(_logBufferPool, GFalse);
         }
-        
     }
     if (file != NULL) {
         listCreate(&_messageList, GTrue);
         GJBufferPoolCreate(&_logBufferPool, 0, GTrue);
-        cb = gj_log_runloop;
+        cb   = gj_log_runloop;
         fmsg = fopen(file, "a+");
         pthread_create(&_logRunloop, GNULL, logRunloop, GNULL);
     }
 }
 
-GVoid GJ_LogSetLevel(GJ_LogLevel level)
-{
+GVoid GJ_LogSetLevel(GJ_LogLevel level) {
 #ifdef GJ_DEBUG
-	defaultDebug = (GJClass*)(debugClass[level]);
-//    RTMP_LogLevel rtmpLevel = (RTMP_LogLevel)level;
-//    GInt avLevel = level;
-//    switch (level) {
-//        case GJ_LOGNONE:
-//            avLevel = AV_LOG_QUIET;
-//            break;
-//        case GJ_LOGFORBID:
-//            avLevel = AV_LOG_PANIC;
-//            break;
-//        case GJ_LOGERROR:
-//            avLevel = AV_LOG_ERROR;
-//            break;
-//        case GJ_LOGWARNING:
-//            avLevel = AV_LOG_WARNING;
-//            break;
-//        case GJ_LOGDEBUG:
-//            rtmpLevel = RTMP_LOGDEBUG;
-//            avLevel = AV_LOG_DEBUG;
-//            break;
-//        case GJ_LOGINFO:
-//            rtmpLevel = RTMP_LOGINFO;
-//            avLevel = AV_LOG_TRACE;
-//            break;
-//        case GJ_LOGALL:
-//
-//            break;
-//        default:
-//            break;
-//    }
-    
+    defaultDebug = (GJClass *) (debugClass[level]);
+    //    RTMP_LogLevel rtmpLevel = (RTMP_LogLevel)level;
+    //    GInt avLevel = level;
+    //    switch (level) {
+    //        case GJ_LOGNONE:
+    //            avLevel = AV_LOG_QUIET;
+    //            break;
+    //        case GJ_LOGFORBID:
+    //            avLevel = AV_LOG_PANIC;
+    //            break;
+    //        case GJ_LOGERROR:
+    //            avLevel = AV_LOG_ERROR;
+    //            break;
+    //        case GJ_LOGWARNING:
+    //            avLevel = AV_LOG_WARNING;
+    //            break;
+    //        case GJ_LOGDEBUG:
+    //            rtmpLevel = RTMP_LOGDEBUG;
+    //            avLevel = AV_LOG_DEBUG;
+    //            break;
+    //        case GJ_LOGINFO:
+    //            rtmpLevel = RTMP_LOGINFO;
+    //            avLevel = AV_LOG_TRACE;
+    //            break;
+    //        case GJ_LOGALL:
+    //
+    //            break;
+    //        default:
+    //            break;
+    //    }
+
     RTMP_LogSetLevel(RTMP_LOGWARNING);
     av_log_set_level(AV_LOG_WARNING);
-    
+
 #else
-    if ( !fmsg ) fmsg = stderr;
+    if (!fmsg) fmsg = stderr;
     fprintf(fmsg, "ERROR:%s设置错误，请重新编译GJLiveLog,并在此之前#define GJ_DEBUG\n", levels[level]);
     fflush(fmsg);
 #endif
 }
 
-GVoid GJ_LogSetCallback(GJ_LogCallback *cbp)
-{
-	cb = cbp;
+GVoid GJ_LogSetCallback(GJ_LogCallback *cbp) {
+    cb = cbp;
 }
 
-GJ_LogLevel GJ_LogGetLevel()
-{
-	return defaultDebug->dLevel;
+GJ_LogLevel GJ_LogGetLevel() {
+    return defaultDebug->dLevel;
 }
 
-inline GVoid GJ_Log(const GVoid* logClass, GJ_LogLevel level,const char *pre,const char *format, ...)
-{
+inline GVoid GJ_Log(const GVoid *logClass, GJ_LogLevel level, const char *pre, const char *format, ...) {
 #ifdef GJ_DEBUG
-    GJClass* dClass = defaultDebug;
+    GJClass *dClass = defaultDebug;
     if (logClass != GNULL) {
-        dClass = (GJClass*)logClass;
+        dClass = (GJClass *) logClass;
     }
-    if ( level <= dClass->dLevel ) {
+    if (level <= dClass->dLevel) {
         va_list args;
         va_start(args, format);
-        cb(dClass,level, pre, format, args);
+        cb(dClass, level, pre, format, args);
         va_end(args);
     }
 #endif
@@ -202,147 +192,146 @@ inline GVoid GJ_Log(const GVoid* logClass, GJ_LogLevel level,const char *pre,con
 
 static const char hexdig[] = "0123456789abcdef";
 
-inline GVoid GJ_LogHex(GJ_LogLevel level, const GUInt8 *data, GUInt32 len)
-{
+inline GVoid GJ_LogHex(GJ_LogLevel level, const GUInt8 *data, GUInt32 len) {
 #ifdef GJ_DEBUG
 
-	GUInt32 i;
-	char line[50], *ptr;
+    GUInt32 i;
+    char    line[50], *ptr;
 
-	if ( level > defaultDebug->dLevel )
-		return;
+    if (level > defaultDebug->dLevel)
+        return;
 
-	ptr = line;
+    ptr = line;
 
-	for(i=0; i<len; i++) {
-		*ptr++ = hexdig[0x0f & (data[i] >> 4)];
-		*ptr++ = hexdig[0x0f & data[i]];
-		if ((i & 0x0f) == 0x0f) {
-			*ptr = '\0';
-			ptr = line;
-            GJ_Log(defaultDebug,level, "GJ_LogHex","%s", (char*)line);
-		} else {
-			*ptr++ = ' ';
-		}
-	}
-	if (i & 0x0f) {
-		*ptr = '\0';
-		GJ_Log(defaultDebug,level,"GJ_LogHex", "%s", line);
-	}
+    for (i = 0; i < len; i++) {
+        *ptr++ = hexdig[0x0f & (data[i] >> 4)];
+        *ptr++ = hexdig[0x0f & data[i]];
+        if ((i & 0x0f) == 0x0f) {
+            *ptr = '\0';
+            ptr  = line;
+            GJ_Log(defaultDebug, level, "GJ_LogHex", "%s", (char *) line);
+        } else {
+            *ptr++ = ' ';
+        }
+    }
+    if (i & 0x0f) {
+        *ptr = '\0';
+        GJ_Log(defaultDebug, level, "GJ_LogHex", "%s", line);
+    }
 #endif
 }
 
-inline GVoid GJ_LogHexString(GJ_LogLevel level, const GUInt8 *data, GUInt32 len)
-{
+inline GVoid GJ_LogHexString(GJ_LogLevel level, const GUInt8 *data, GUInt32 len) {
 #define BP_OFFSET 9
 #define BP_GRAPH 60
-#define BP_LEN	80
-    
+#define BP_LEN 80
+
 #ifdef GJ_DEBUG
-	char	line[BP_LEN];
-	GUInt32 i;
+    char    line[BP_LEN];
+    GUInt32 i;
 
-	if ( !data || level > defaultDebug->dLevel )
-		return;
+    if (!data || level > defaultDebug->dLevel)
+        return;
 
-	/* in case len is zero */
-	line[0] = '\0';
+    /* in case len is zero */
+    line[0] = '\0';
 
-	for ( i = 0 ; i < len ; i++ ) {
-		GInt32 n = i % 16;
-		unsigned off;
+    for (i = 0; i < len; i++) {
+        GInt32   n = i % 16;
+        unsigned off;
 
-		if( !n ) {
-			if( i ) GJ_Log(defaultDebug, level,"GJ_LogHexString", "%s", line );
-			memset( line, ' ', sizeof(line)-2 );
-			line[sizeof(line)-2] = '\0';
+        if (!n) {
+            if (i) GJ_Log(defaultDebug, level, "GJ_LogHexString", "%s", line);
+            memset(line, ' ', sizeof(line) - 2);
+            line[sizeof(line) - 2] = '\0';
 
-			off = i % 0x0ffffU;
+            off = i % 0x0ffffU;
 
-			line[2] = hexdig[0x0f & (off >> 12)];
-			line[3] = hexdig[0x0f & (off >>  8)];
-			line[4] = hexdig[0x0f & (off >>  4)];
-			line[5] = hexdig[0x0f & off];
-			line[6] = ':';
-		}
+            line[2] = hexdig[0x0f & (off >> 12)];
+            line[3] = hexdig[0x0f & (off >> 8)];
+            line[4] = hexdig[0x0f & (off >> 4)];
+            line[5] = hexdig[0x0f & off];
+            line[6] = ':';
+        }
 
-		off = BP_OFFSET + n*3 + ((n >= 8)?1:0);
-		line[off] = hexdig[0x0f & ( data[i] >> 4 )];
-		line[off+1] = hexdig[0x0f & data[i]];
+        off           = BP_OFFSET + n * 3 + ((n >= 8) ? 1 : 0);
+        line[off]     = hexdig[0x0f & (data[i] >> 4)];
+        line[off + 1] = hexdig[0x0f & data[i]];
 
-		off = BP_GRAPH + n + ((n >= 8)?1:0);
+        off = BP_GRAPH + n + ((n >= 8) ? 1 : 0);
 
-		if ( isprint( data[i] )) {
-			line[BP_GRAPH + n] = data[i];
-		} else {
-			line[BP_GRAPH + n] = '.';
-		}
-	}
+        if (isprint(data[i])) {
+            line[BP_GRAPH + n] = data[i];
+        } else {
+            line[BP_GRAPH + n] = '.';
+        }
+    }
 
-	GJ_Log(defaultDebug, level,"GJ_LogHexString", "%s", line );
+    GJ_Log(defaultDebug, level, "GJ_LogHexString", "%s", line);
 #endif
 }
 
-GVoid GJ_LogAssert(GInt32 isTrue,const char *pre,const char *format, ...){
+GVoid GJ_LogAssert(GInt32 isTrue, const char *pre, const char *format, ...) {
     if (isTrue == 0) {
-        char str[MAX_PRINT_LEN]="";
-        char* fPre  = str;
-        GULong strLen = MAX_PRINT_LEN-1;
+        char   str[MAX_PRINT_LEN] = "";
+        char * fPre               = str;
+        GULong strLen             = MAX_PRINT_LEN - 1;
         if (pre) {
             GULong preLen = strlen(pre);
             strcpy(str, pre);
-            str[preLen++]=':';
-            fPre = str + preLen;
+            str[preLen++] = ':';
+            fPre          = str + preLen;
             strLen -= preLen;
         }
 
-        GInt32 len;
+        GInt32  len;
         va_list args;
         va_start(args, format);
         len = vsnprintf(fPre, strLen, format, args);
         va_end(args);
- 
+
         fprintf(fmsg, "%s\n", str);
         fflush(fmsg);
-        if ( defaultDebug->dLevel >= GJ_LOGDEBUG)
+        if (defaultDebug->dLevel >= GJ_LOGDEBUG)
             assert(0);
     }
 }
 
-GBool GJ_LogCheckResult(GResult result,const char *pre,const char *format, ...){
+GBool GJ_LogCheckResult(GResult result, const char *pre, const char *format, ...) {
     if (result != GOK && defaultDebug->dLevel > GJ_LOGNONE) {
 
-        char str[MAX_PRINT_LEN]="";
+        char    str[MAX_PRINT_LEN] = "";
         va_list args;
         va_start(args, format);
-        vsnprintf(str, MAX_PRINT_LEN-1, format, args);
+        vsnprintf(str, MAX_PRINT_LEN - 1, format, args);
         va_end(args);
-        
-        if ( !fmsg ) fmsg = stderr;
+
+        if (!fmsg) fmsg = stderr;
         struct tm *local;
-        time_t t;
-        t=time(NULL);
-        local=localtime(&t);
-        fprintf(fmsg, "[%02d:%02d:%02d][GJ_LOGCHECK][%s]:%s error result:%d\n",local->tm_hour,local->tm_min,local->tm_sec,pre,str,result);
+        time_t     t;
+        t     = time(NULL);
+        local = localtime(&t);
+        fprintf(fmsg, "[%02d:%02d:%02d][GJ_LOGCHECK][%s]:%s error result:%d\n", local->tm_hour, local->tm_min, local->tm_sec, pre, str, result);
         fflush(fmsg);
     }
     return result == GOK;
 }
-GBool GJ_LogCheckBool(GBool isTrue,const char *pre,const char *format, ...){
+
+GBool GJ_LogCheckBool(GBool isTrue, const char *pre, const char *format, ...) {
     if (!isTrue && defaultDebug->dLevel > GJ_LOGNONE) {
-        
-        char str[MAX_PRINT_LEN]="";
+
+        char    str[MAX_PRINT_LEN] = "";
         va_list args;
         va_start(args, format);
-        vsnprintf(str, MAX_PRINT_LEN-1, format, args);
+        vsnprintf(str, MAX_PRINT_LEN - 1, format, args);
         va_end(args);
-        
-        if ( !fmsg ) fmsg = stderr;
+
+        if (!fmsg) fmsg = stderr;
         struct tm *local;
-        time_t t;
-        t=time(NULL);
-        local=localtime(&t);
-        fprintf(fmsg, "[%02d:%02d:%02d][GJ_LOGCHECK][%s]:%s error\n",local->tm_hour,local->tm_min,local->tm_sec,pre,str);
+        time_t     t;
+        t     = time(NULL);
+        local = localtime(&t);
+        fprintf(fmsg, "[%02d:%02d:%02d][GJ_LOGCHECK][%s]:%s error\n", local->tm_hour, local->tm_min, local->tm_sec, pre, str);
         fflush(fmsg);
     }
     return isTrue;
--- GJLiveEngine/CrossPlatform/GJLog.h
+++ GJLiveEngine/CrossPlatform/GJLog.h
@@ -31,122 +31,115 @@
 #ifdef __cplusplus
 extern "C" {
 #endif
-    /* Enable this to get full debugging */
-    /* #define _DEBUG */
-    
+/* Enable this to get full debugging */
+/* #define _DEBUG */
+
 #define GJ_DEBUG
 
 #define DEFAULT_LOG GNULL
-    
-#define GJ_DEBUG_FREQUENTLY
-    
 
-    
-    typedef enum
-    {
-        GJ_LOGNONE,///永远不产生log
-        GJ_LOGFORBID,//调试包会产生中断，
-        GJ_LOGERROR,
-        GJ_LOGWARNING,
-        GJ_LOGDEBUG,
-        GJ_LOGINFO,
-        GJ_LOGALL
-    } GJ_LogLevel;
-    
-    
-    typedef struct _GJClass {
-        GChar* className;
-        GJ_LogLevel dLevel;
-    }GJClass;
-    
-    static const GJClass Default_NONE = {
-        .className = GNULL,
-        .dLevel    = GJ_LOGNONE,
-    };
+#define GJ_DEBUG_FREQUENTLY
+
+typedef enum {
+    GJ_LOGNONE,   ///永远不产生log
+    GJ_LOGFORBID, //调试包会产生中断，
+    GJ_LOGERROR,
+    GJ_LOGWARNING,
+    GJ_LOGDEBUG,
+    GJ_LOGINFO,
+    GJ_LOGALL
+} GJ_LogLevel;
+
+typedef struct _GJClass {
+    GChar *     className;
+    GJ_LogLevel dLevel;
+} GJClass;
+
+static const GJClass Default_NONE = {
+    .className = GNULL,
+    .dLevel    = GJ_LOGNONE,
+};
 #define LOG_NONE (&Default_NONE)
-    
-    static const GJClass Default_FORBID = {
-        .className = GNULL,
-        .dLevel    = GJ_LOGFORBID,
-    };
-    
+
+static const GJClass Default_FORBID = {
+    .className = GNULL,
+    .dLevel    = GJ_LOGFORBID,
+};
+
 #define LOG_FORBID (&Default_FORBID)
 
-    static const GJClass Default_ERROR = {
-        .className = GNULL,
-        .dLevel    = GJ_LOGERROR,
-    };
+static const GJClass Default_ERROR = {
+    .className = GNULL,
+    .dLevel    = GJ_LOGERROR,
+};
 #define LOG_ERROR (&Default_ERROR)
 
-    static const GJClass Default_WARNING = {
-        .className = GNULL,
-        .dLevel    = GJ_LOGWARNING,
-    };
+static const GJClass Default_WARNING = {
+    .className = GNULL,
+    .dLevel    = GJ_LOGWARNING,
+};
 #define LOG_WARNING (&Default_WARNING)
 
-    static const GJClass Default_DEBUG = {
-        .className = GNULL,
-        .dLevel    = GJ_LOGDEBUG,
-    };
+static const GJClass Default_DEBUG = {
+    .className = GNULL,
+    .dLevel    = GJ_LOGDEBUG,
+};
 #define LOG_DEBUG (&Default_DEBUG)
 
-    static const GJClass Default_INFO = {
-        .className = GNULL,
-        .dLevel    = GJ_LOGINFO,
-    };
+static const GJClass Default_INFO = {
+    .className = GNULL,
+    .dLevel    = GJ_LOGINFO,
+};
 #define LOG_INFO (&Default_INFO)
 
-    static const GJClass Default_ALL = {
-        .className = GNULL,
-        .dLevel    = GJ_LOGALL,
-    };
+static const GJClass Default_ALL = {
+    .className = GNULL,
+    .dLevel    = GJ_LOGALL,
+};
 #define LOG_ALL (&Default_ALL)
 
-    extern GJClass* defaultDebug;
-    
-    
-    typedef GVoid (GJ_LogCallback)(GJClass* logClass, GJ_LogLevel level, const char *pre, const char *fmt, va_list);
-    
-    //小于GJ_debuglevel则显示
-    GVoid GJ_LogSetLevel(GJ_LogLevel lvl);
-    
-    GVoid GJ_LogSetCallback(GJ_LogCallback *cb);
-    GVoid GJ_LogSetOutput(char *file);
+extern GJClass *defaultDebug;
 
-    GVoid GJ_Log(const GVoid* logClass, GJ_LogLevel level,const char *pre,const char *format, ...) __printflike(4, 5);
-    GVoid GJ_LogHex(GJ_LogLevel level, const GUInt8 *data, GUInt32 len);
-    GVoid GJ_LogHexString(GJ_LogLevel level, const GUInt8 *data, GUInt32 len);
-    
-    //所有等级都会打印，但是大于GJ_LOGDEBUG模式会产生中断
-    GVoid GJ_LogAssert(GInt32 isTrue,const char *pre,const char *format, ...);
-    GBool GJ_LogCheckResult(GResult result,const char *pre,const char *format, ...);
-    GBool GJ_LogCheckBool(GBool result,const char *pre,const char *format, ...);
+typedef GVoid(GJ_LogCallback)(GJClass *logClass, GJ_LogLevel level, const char *pre, const char *fmt, va_list);
 
-    GJ_LogLevel GJ_LogGetLevel(GVoid);
-    
-    
-    
-#define GJCheckResult(isTrue, format, ...)  GJ_LogCheckResult(isTrue,__func__,format,##__VA_ARGS__)
-#define GJCheckBool(isTrue, format, ...)  GJ_LogCheckBool(isTrue,__func__,format,##__VA_ARGS__)
+//小于GJ_debuglevel则显示
+GVoid GJ_LogSetLevel(GJ_LogLevel lvl);
 
-#define GJAssert(isTrue, format, ...) GJ_LogAssert(isTrue,__func__,format,##__VA_ARGS__)
-    
+GVoid GJ_LogSetCallback(GJ_LogCallback *cb);
+GVoid GJ_LogSetOutput(char *file);
+
+GVoid GJ_Log(const GVoid *logClass, GJ_LogLevel level, const char *pre, const char *format, ...) __printflike(4, 5);
+GVoid GJ_LogHex(GJ_LogLevel level, const GUInt8 *data, GUInt32 len);
+GVoid GJ_LogHexString(GJ_LogLevel level, const GUInt8 *data, GUInt32 len);
+
+//所有等级都会打印，但是大于GJ_LOGDEBUG模式会产生中断
+GVoid GJ_LogAssert(GInt32 isTrue, const char *pre, const char *format, ...);
+GBool GJ_LogCheckResult(GResult result, const char *pre, const char *format, ...);
+GBool GJ_LogCheckBool(GBool result, const char *pre, const char *format, ...);
+
+GJ_LogLevel GJ_LogGetLevel(GVoid);
+
+#define GJCheckResult(isTrue, format, ...) GJ_LogCheckResult(isTrue, __func__, format, ##__VA_ARGS__)
+#define GJCheckBool(isTrue, format, ...) GJ_LogCheckBool(isTrue, __func__, format, ##__VA_ARGS__)
+
+#define GJAssert(isTrue, format, ...) GJ_LogAssert(isTrue, __func__, format, ##__VA_ARGS__)
+
 #ifdef GJ_DEBUG
-    
-#define GJLOG(dclass,level,format, ...) GJ_Log((dclass),(level),__func__,format,##__VA_ARGS__)
-    
+
+#define GJLOG(dclass, level, format, ...) GJ_Log((dclass), (level), __func__, format, ##__VA_ARGS__)
+
 #ifdef GJ_DEBUG_FREQUENTLY
-    
-#define GJLOGFREQ(format, ...) GJ_Log(DEFAULT_LOG,GJ_LOGALL,__func__,format,##__VA_ARGS__)
+
+#define GJLOGFREQ(format, ...) GJ_Log(DEFAULT_LOG, GJ_LOGALL, __func__, format, ##__VA_ARGS__)
 #else
-    
-#define GJLOGFREQ(level,format, ...)
+
+#define GJLOGFREQ(level, format, ...)
 #endif
-    
+
 #else
-#define GJLOG(dclass,level, format, ...)
-    #define GJOLOG(switch,level,format, ...)
-#define GJLOGFREQ(level,format, ...)
+#define GJLOG(dclass, level, format, ...)
+#define GJOLOG(switch, level, format, ...)
+#define GJLOGFREQ(level, format, ...)
 #endif
     
     
