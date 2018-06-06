bdae0ecf1d0c1c5f8c46c5142bc6411e0a3d347b
melot <zhouguangjin@zhouguangjindeiMac.local>
2018-06-06 15:12:13 +0800

    注释

--- GJLiveEngine/CrossPlatform/GJQueue/GJRetainBuffer.h
+++ GJLiveEngine/CrossPlatform/GJQueue/GJRetainBuffer.h
@@ -13,7 +13,6 @@
 #include <string.h>
 #include "GJLog.h"
 
-
 #include "GJPlatformHeader.h"
 #if MEMORY_CHECK
 #include "GJList.h"
@@ -22,184 +21,185 @@
 #ifdef __cplusplus
 extern "C" {
 #endif
-    
+
 typedef struct _GJRetainBuffer GJRetainBuffer;
 
-typedef GBool (*RetainReleaseCallBack)(struct _GJRetainBuffer* data);
-    
-    struct _GJRetainBuffer{
-        GInt32 capacity; //data之后的实际内存大小
-        GInt32 size;      //data之后的实际使用内存大小，剩下之后的一定没有使用
-        GInt32 frontSize; //data之前的内存,所以外部释放时一定要注意free((char*)data-frontSize);
-        GInt32 retainCount;
-        GUInt8* data;
-#if MEMORY_CHECK
-        GBool needCheck;
-        GJList* retainList;//retain调用的函数名
-        GJList* unretainList;//unretain调用的函数名
-#endif
-        RetainReleaseCallBack retainReleaseCallBack;
-        GVoid *parm;
-    };
-    
-#if MEMORY_CHECK
-    GVoid R_BufferMemCheck(GJRetainBuffer* buffer);
-#endif
-    
-#if 1
-    static inline GUInt8* R_BufferOrigin(GJRetainBuffer* buffer){
-#if MEMORY_CHECK
-        return buffer->data-buffer->frontSize;
-#else
-        return buffer->data-buffer->frontSize;
-#endif
-    }
-    
-    static inline GUInt8* _R_BufferStart(GJRetainBuffer* buffer){
-        return buffer->data;
-    }
-#define R_BufferStart(buffer) _R_BufferStart((GJRetainBuffer*)(buffer))
-    
-    static inline GUInt8* R_BufferCurrent(GJRetainBuffer* buffer){
-        return buffer->data+buffer->size;
-    }
-    
-    static inline GUInt8* R_BufferEnd(GJRetainBuffer* buffer){
-        return buffer->data+buffer->capacity;
-    }
-    
-    static inline GVoid _R_BufferUseSize(GJRetainBuffer* buffer,GInt32 size){
-        buffer->size += size;
-        GJAssert((buffer->size <= buffer->capacity),"MEM ERROR");
-    }
-#define R_BufferUseSize(buffer,size) _R_BufferUseSize((GJRetainBuffer*)(buffer),size)
-    static inline GInt32 _R_BufferSize(GJRetainBuffer* buffer){
-        return buffer->size;
-    }
-#define R_BufferSize(buffer) _R_BufferSize((GJRetainBuffer*)(buffer))
+typedef GBool (*RetainReleaseCallBack)(struct _GJRetainBuffer *data);
 
-    static inline GInt32 R_BufferFrontSize(GJRetainBuffer* buffer){
-        return buffer->frontSize;
-    }
+struct _GJRetainBuffer {
+    GInt32  capacity;  //data之后的实际内存大小
+    GInt32  size;      //data之后的实际使用内存大小，剩下之后的一定没有使用
+    GInt32  frontSize; //data之前的内存,所以外部释放时一定要注意free((char*)data-frontSize);
+    GInt32  retainCount;
+    GUInt8 *data;
+#if MEMORY_CHECK
+    GBool   needCheck;
+    GJList *retainList;   //retain调用的函数名
+    GJList *unretainList; //unretain调用的函数名
+#endif
+    RetainReleaseCallBack retainReleaseCallBack;
+    GVoid *               parm;
+};
+
+#if MEMORY_CHECK
+GVoid R_BufferMemCheck(GJRetainBuffer *buffer);
+#endif
+
+#if 1
+static inline GUInt8 *R_BufferOrigin(GJRetainBuffer *buffer) {
+#if MEMORY_CHECK
+    return buffer->data - buffer->frontSize;
+#else
+    return buffer->data - buffer->frontSize;
+#endif
+}
+
+static inline GUInt8 *_R_BufferStart(GJRetainBuffer *buffer) {
+    return buffer->data;
+}
+#define R_BufferStart(buffer) _R_BufferStart((GJRetainBuffer *) (buffer))
+
+static inline GUInt8 *R_BufferCurrent(GJRetainBuffer *buffer) {
+    return buffer->data + buffer->size;
+}
+
+static inline GUInt8 *R_BufferEnd(GJRetainBuffer *buffer) {
+    return buffer->data + buffer->capacity;
+}
+
+static inline GVoid _R_BufferUseSize(GJRetainBuffer *buffer, GInt32 size) {
+    buffer->size += size;
+    GJAssert((buffer->size <= buffer->capacity), "MEM ERROR");
+}
+#define R_BufferUseSize(buffer, size) _R_BufferUseSize((GJRetainBuffer *) (buffer), size)
     
-    static inline GInt32 R_BufferCapacity(GJRetainBuffer* buffer){
-        return buffer->capacity;
-    }
-    
-    static inline GInt32  R_BufferRetainCount(GJRetainBuffer* buffer){
-        return buffer->retainCount;
-    }
-    
-    static inline GHandle  R_BufferUserData(GJRetainBuffer* buffer){
-        return buffer->parm;
-    }
-    
-    static inline GVoid R_BufferSetCallback(GJRetainBuffer* buffer,RetainReleaseCallBack callback){
-        buffer->retainReleaseCallBack = callback;
-    }
+static inline GInt32 _R_BufferSize(GJRetainBuffer *buffer) {
+    return buffer->size;
+}
+#define R_BufferSize(buffer) _R_BufferSize((GJRetainBuffer *) (buffer))
+
+static inline GInt32 R_BufferFrontSize(GJRetainBuffer *buffer) {
+    return buffer->frontSize;
+}
+
+static inline GInt32 R_BufferCapacity(GJRetainBuffer *buffer) {
+    return buffer->capacity;
+}
+
+static inline GInt32 R_BufferRetainCount(GJRetainBuffer *buffer) {
+    return buffer->retainCount;
+}
+
+static inline GHandle R_BufferUserData(GJRetainBuffer *buffer) {
+    return buffer->parm;
+}
+
+static inline GVoid R_BufferSetCallback(GJRetainBuffer *buffer, RetainReleaseCallBack callback) {
+    buffer->retainReleaseCallBack = callback;
+}
 //    将内存制成某个常量
-    static inline GVoid  R_BufferWriteConst(GJRetainBuffer* buffer,GInt data,GInt32 size){
-        GJAssert(buffer->size + size <= buffer->capacity, "MEM ERROR");
-        memset(buffer->data+buffer->size, data, size);
-        buffer->size += size;
-    }
-    
-    static inline GVoid  _R_BufferWrite(GJRetainBuffer* buffer,GUInt8* data,GInt32 size){
-        GJAssert(buffer->size + size <= buffer->capacity, "MEM ERROR");
-        memcpy(buffer->data+buffer->size, data, size);
-        buffer->size += size;
-    }
-    
-#define R_BufferWrite(buffer,data,size) _R_BufferWrite((GJRetainBuffer*)buffer,(GUInt8*)data,size);
+static inline GVoid R_BufferWriteConst(GJRetainBuffer *buffer, GInt data, GInt32 size) {
+    GJAssert(buffer->size + size <= buffer->capacity, "MEM ERROR");
+    memset(buffer->data + buffer->size, data, size);
+    buffer->size += size;
+}
+
+static inline GVoid _R_BufferWrite(GJRetainBuffer *buffer, GUInt8 *data, GInt32 size) {
+    GJAssert(buffer->size + size <= buffer->capacity, "MEM ERROR");
+    memcpy(buffer->data + buffer->size, data, size);
+    buffer->size += size;
+}
+
+#define R_BufferWrite(buffer, data, size) _R_BufferWrite((GJRetainBuffer *) buffer, (GUInt8 *) data, size);
 //    static inline GVoid  R_BufferRead(GJRetainBuffer* buffer,GUInt8* data,GInt32 size){
 //        GJAssert(buffer->size >= size, "MEM ERROR");
 //        memcpy(data, buffer->data+buffer->size-size, size);
 //        buffer->size -= size;
 //    }
 
-    static inline GVoid R_BufferClearFront(GJRetainBuffer* buffer){
-        if (buffer->frontSize>0) {
-            buffer->data = buffer->data-buffer->frontSize;
-            buffer->capacity = buffer->capacity + buffer->frontSize;
-            buffer->frontSize = 0;
-        }
-        buffer->size = 0;
+static inline GVoid R_BufferClearFront(GJRetainBuffer *buffer) {
+    if (buffer->frontSize > 0) {
+        buffer->data      = buffer->data - buffer->frontSize;
+        buffer->capacity  = buffer->capacity + buffer->frontSize;
+        buffer->frontSize = 0;
     }
-    static inline GVoid R_BufferClearSize(GJRetainBuffer* buffer){
-        buffer->size = 0;
-    }
-    
+    buffer->size = 0;
+}
+static inline GVoid R_BufferClearSize(GJRetainBuffer *buffer) {
+    buffer->size = 0;
+}
+
 /**
  起死回生，区别与_R_BufferRetain，明确知道retainCount==0，需要重新复活+1；
 
  @param x x description
  @return return value description
  */
-#define R_BufferRelive(x) _R_BufferRelive(x,__func__)
-    static inline GVoid _R_BufferRelive(GJRetainBuffer* buffer, const GChar* tracker DEFAULT_PARAM(GNull)){
-        __sync_fetch_and_add(&buffer->retainCount,1);
+#define R_BufferRelive(x) _R_BufferRelive(x, __func__)
+static inline GVoid _R_BufferRelive(GJRetainBuffer *buffer, const GChar *tracker DEFAULT_PARAM(GNull)) {
+    __sync_fetch_and_add(&buffer->retainCount, 1);
 #if MEMORY_CHECK
-        GJAssert(buffer->retainCount == 1, "");
+    GJAssert(buffer->retainCount == 1, "");
 
-        if(buffer->retainList == GNULL){
-            listCreate(&buffer->retainList, GTrue);
-            listCreate(&buffer->unretainList, GTrue);
-        }
-        listPush(buffer->retainList, (GHandle)tracker);
-#endif
+    if (buffer->retainList == GNULL) {
+        listCreate(&buffer->retainList, GTrue);
+        listCreate(&buffer->unretainList, GTrue);
     }
-    /**
+    listPush(buffer->retainList, (GHandle) tracker);
+#endif
+}
+/**
      引用计数加1
-     
+ 
      @param buffer buffer description
      */
 //    GVoid _R_BufferRetain(GJRetainBuffer* buffer, const GChar* tracker DEFAULT_PARAM(GNull));
-    #define R_BufferRetain(x) _R_BufferRetain((GJRetainBuffer*)(x),__func__)
+#define R_BufferRetain(x) _R_BufferRetain((GJRetainBuffer *) (x), __func__)
 
-    static inline GVoid _R_BufferRetain(GJRetainBuffer* buffer, const GChar* tracker DEFAULT_PARAM(GNull)){
-        
-         __sync_fetch_and_add(&buffer->retainCount,1);
+static inline GVoid _R_BufferRetain(GJRetainBuffer *buffer, const GChar *tracker DEFAULT_PARAM(GNull)) {
+
+    __sync_fetch_and_add(&buffer->retainCount, 1);
 #if MEMORY_CHECK
-        GJAssert(buffer->retainCount > 1, "0及以下的retaincount不能retain");
-        if(buffer->retainList == GNULL){
-            listCreate(&buffer->retainList, GTrue);
-            listCreate(&buffer->unretainList, GTrue);
-        }
-        listPush(buffer->retainList, (GHandle)tracker);
-#endif
+    GJAssert(buffer->retainCount > 1, "0及以下的retaincount不能retain");
+    if (buffer->retainList == GNULL) {
+        listCreate(&buffer->retainList, GTrue);
+        listCreate(&buffer->unretainList, GTrue);
     }
+    listPush(buffer->retainList, (GHandle) tracker);
+#endif
+}
 #else
-    GUInt8* R_BufferOrigin(GJRetainBuffer* buffer);
-    
-    GUInt8* R_BufferStart(GJRetainBuffer* buffer);
-    
-    GUInt8* R_BufferCurrent(GJRetainBuffer* buffer);
-    
-    GUInt8* R_BufferEnd(GJRetainBuffer* buffer);
-    
-    GVoid R_BufferSetSize(GJRetainBuffer* buffer,GInt32 size);
+GUInt8 *R_BufferOrigin(GJRetainBuffer *buffer);
 
-    GInt32 R_BufferSize(GJRetainBuffer* buffer);
-    
-    GInt32 R_BufferFrontSize(GJRetainBuffer* buffer);
+GUInt8 *R_BufferStart(GJRetainBuffer *buffer);
 
-    GInt32 R_BufferCapacity(GJRetainBuffer* buffer);
-    
-    GInt32  R_BufferRetainCount(GJRetainBuffer* buffer);
-    
-    GHandle  R_BufferUserData(GJRetainBuffer* buffer);
-    
-    GVoid R_BufferSetCallback(GJRetainBuffer* buffer,RetainReleaseCallBack callback);
-    
-    GVoid  R_BufferWrite(GJRetainBuffer* buffer,GUInt8* data,GInt32 size);
-    
-    GVoid  R_BufferWriteAppend(GJRetainBuffer* buffer,GUInt8* data,GInt32 size);
-    
-    GVoid  R_BufferEraseOut(GJRetainBuffer* buffer,GUInt8* data,GInt32 size);
-    
-    GVoid R_BufferClearFront(GJRetainBuffer* buffer);
-    
-    GVoid R_BufferRetain(GJRetainBuffer* buffer);
+GUInt8 *R_BufferCurrent(GJRetainBuffer *buffer);
+
+GUInt8 *R_BufferEnd(GJRetainBuffer *buffer);
+
+GVoid R_BufferSetSize(GJRetainBuffer *buffer, GInt32 size);
+
+GInt32 R_BufferSize(GJRetainBuffer *buffer);
+
+GInt32 R_BufferFrontSize(GJRetainBuffer *buffer);
+
+GInt32 R_BufferCapacity(GJRetainBuffer *buffer);
+
+GInt32 R_BufferRetainCount(GJRetainBuffer *buffer);
+
+GHandle R_BufferUserData(GJRetainBuffer *buffer);
+
+GVoid R_BufferSetCallback(GJRetainBuffer *buffer, RetainReleaseCallBack callback);
+
+GVoid R_BufferWrite(GJRetainBuffer *buffer, GUInt8 *data, GInt32 size);
+
+GVoid R_BufferWriteAppend(GJRetainBuffer *buffer, GUInt8 *data, GInt32 size);
+
+GVoid R_BufferEraseOut(GJRetainBuffer *buffer, GUInt8 *data, GInt32 size);
+
+GVoid R_BufferClearFront(GJRetainBuffer *buffer);
+
+GVoid R_BufferRetain(GJRetainBuffer *buffer);
 
 #endif
 
@@ -213,8 +213,8 @@ typedef GBool (*RetainReleaseCallBack)(struct _GJRetainBuffer* data);
  @param releaseCallBack releaseCallBack不为空时，在 retainCount为0时调用，并且在releaseCallBack中释放data内存。
                         返回值表示releaseCallBack中是否已经释放GJRetainBuffer,当releaseCallBack为空时，GJRetainBuffer和data都在retainCount为0时自动释放。
  */
-GVoid R_BufferAlloc(GJRetainBuffer** R_Buffer,GInt32 size,GBool (*releaseCallBack)(GJRetainBuffer* data),GVoid* parm );
-    
+GVoid R_BufferAlloc(GJRetainBuffer **R_Buffer, GInt32 size, GBool (*releaseCallBack)(GJRetainBuffer *data), GVoid *parm);
+
 /**
  打包已经存在的内存块.注意该模块不参与内存校验
 
@@ -223,34 +223,32 @@ GVoid R_BufferAlloc(GJRetainBuffer** R_Buffer,GInt32 size,GBool (*releaseCallBac
  @param size size description
  @param releaseCallBack releaseCallBack description
  */
-    
-GVoid R_BufferPack(GJRetainBuffer** R_Buffer,GVoid* data, GInt32 size,GBool (*releaseCallBack)(GJRetainBuffer* data),GVoid* parm);
-    
+
+GVoid R_BufferPack(GJRetainBuffer **R_Buffer, GVoid *data, GInt32 size, GBool (*releaseCallBack)(GJRetainBuffer *data), GVoid *parm);
+
 /**
  不考虑releaseCallback,直接释放data内存，但是不释放buffer内存；
 
  @param buffer buffer description
  */
-GVoid R_BufferFreeData(GJRetainBuffer* buffer);
-    
+GVoid R_BufferFreeData(GJRetainBuffer *buffer);
+
 /**
  需要扩大内存，一定要用此接口。只影响capacity，不保证内容不变。frontSize、size保持不变。
  
  @param retainbuffer retainbuffer description
  @param size 指capacity扩大到多少，不改变frontSize，所以总内存大小为frontSize+capacity。只扩大，缩小则没有影响；
  */
-GVoid R_BufferReCapacity(GJRetainBuffer* retainbuffer,GInt32 size);
-    
+GVoid R_BufferReCapacity(GJRetainBuffer *retainbuffer, GInt32 size);
 
 /**
  引用计数减1
 
  @param buffer buffer description
  */
-GVoid _R_BufferUnRetain(GJRetainBuffer* buffer, const GChar* tracker DEFAULT_PARAM(GNull));
-#define R_BufferUnRetain(x) _R_BufferUnRetain((GJRetainBuffer*)(x),__func__)
-    
-    
+GVoid _R_BufferUnRetain(GJRetainBuffer *buffer, const GChar *tracker DEFAULT_PARAM(GNull));
+#define R_BufferUnRetain(x) _R_BufferUnRetain((GJRetainBuffer *) (x), __func__)
+
 GVoid R_BufferUnRetainUnTrack(GHandle buffer);
 
 /**
--- GJLiveEngine/CrossPlatform/GJQueue/GJSignal.h
+++ GJLiveEngine/CrossPlatform/GJQueue/GJSignal.h
@@ -13,13 +13,42 @@
 #include "GJPlatformHeader.h"
 typedef struct _GJSignal GJSignal;
 
-//默认信号是非设置状态,wait不会等待
-GBool signalCreate(GJSignal** signal);
+/**
+ 默认信号是非设置状态,wait不会等待
 
-//表示信号重置状态，wait会阻塞到下一次signal或者超时
-GVoid signalReset(GJSignal* signal);
-GBool signalWait(GJSignal* signal,GUInt32 ms);
-//触发信号，同时信号为触发状态，wait不会阻塞
+ @param signal signal description
+ @return return value description
+ */
+GBool signalCreate(GJSignal **signal);
+
+/**
+ 表示信号重置状态，wait会阻塞到下一次signal或者超时
+ 
+ @param signal signal description
+ */
+GVoid signalReset(GJSignal *signal);
+
+/**
+ 如果是Reset状态，则阻塞等待signalEmit触发，否则不等待
+
+ @param signal signal description
+ @param ms ms description
+ @return false表示等待超时，否则表示不是Reset状态或者已经触发了signal，
+ */
+GBool signalWait(GJSignal *signal, GUInt32 ms);
+
+/**
+ 触发信号，同时信号为触发状态，之后的wait不会阻塞，需要重新reset
+
+ @param signal signal description
+ */
 GVoid signalEmit(GJSignal* signal);
+
+
+/**
+ 销毁不支持多线程所以需要确保没有使用再销毁。
+
+ @param signal signal description
+ */
 GVoid signalDestory(GJSignal** signal);
 #endif /* GJSignal_h */
