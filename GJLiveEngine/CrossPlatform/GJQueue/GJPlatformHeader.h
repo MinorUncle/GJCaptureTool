a72547a4c1b108ebfd7c39699c91968ee3e7c0ec
melot <zhouguangjin@zhouguangjindeiMac.local>
2018-06-11 10:58:45 +0800

    GDoubleEqual

--- GJLiveEngine/CrossPlatform/GJQueue/GJPlatformHeader.h
+++ GJLiveEngine/CrossPlatform/GJQueue/GJPlatformHeader.h
@@ -21,11 +21,8 @@
 
 #define offsetof(t, d) __builtin_offsetof(t, d)
 
-
-
-
-#define GTrue (GInt8)1
-#define GFalse (GInt8)0
+#define GTrue (GInt8) 1
+#define GFalse (GInt8) 0
 #define GNULL NULL
 
 #define GINT8_MAX 127
@@ -45,49 +42,49 @@
 
 #define GINT64_MAX 9223372036854775807LL
 #define GINT64_MIN -9223372036854775807LL
-#define GUINT64_MAX (GINT64_MAX<<1+1)
+#define GUINT64_MAX (GINT64_MAX << 1 + 1)
 #define GUINT64_MIN 0
 
-typedef uint8_t             GUInt8;
-typedef int8_t              GInt8;
-typedef uint16_t            GUInt16;
-typedef int16_t             GInt16;
-typedef uint32_t            GUInt32;
-typedef int32_t             GInt32;
-typedef uint64_t            GUInt64;
-typedef int64_t             GInt64;
-typedef int                 GInt;
+typedef uint8_t  GUInt8;
+typedef int8_t   GInt8;
+typedef uint16_t GUInt16;
+typedef int16_t  GInt16;
+typedef uint32_t GUInt32;
+typedef int32_t  GInt32;
+typedef uint64_t GUInt64;
+typedef int64_t  GInt64;
+typedef int      GInt;
 
-typedef long                GLong;
-typedef unsigned long       GULong;
-typedef float               GFloat;
-typedef double              GDouble;
-typedef GInt8               GBool;
-typedef char                GChar;
-typedef unsigned char       GUChar;
+typedef long          GLong;
+typedef unsigned long GULong;
+typedef float         GFloat;
+typedef double        GDouble;
+typedef GInt8         GBool;
+typedef char          GChar;
+typedef unsigned char GUChar;
 
-typedef void                GVoid;
-typedef void*               GHandle;
+typedef void  GVoid;
+typedef void *GHandle;
 
-typedef size_t              GSize_t;
+typedef size_t GSize_t;
 
-typedef int32_t             GResult;
+typedef int32_t GResult;
 
-#define GOK                 0
-#define GERR_NOMEM          1
-#define GERR_TIMEDOUT       2
+#define GOK 0
+#define GERR_NOMEM -1
+#define GERR_TIMEDOUT -2
 
+#define GMIN(A, B) ({ __typeof__(A) __a = (A); __typeof__(B) __b = (B); __a < __b ? __a : __b; })
+#define GMAX(A, B) ({ __typeof__(A) __a = (A); __typeof__(B) __b = (B); __a < __b ? __b : __a; })
+#define GFloatEqual(A, B) ({GFloat d = (GFloat)A - (GFloat)B;d > -0.000001 && d < 0.000001; })
+#define GDoubleEqual(A, B) ({GDouble d = (GDouble)A - (GDouble)B;d > -0.000000000000001 && d < 0.000000000000001; })
 
-
-#define GMIN(A,B)	({ __typeof__(A) __a = (A); __typeof__(B) __b = (B); __a < __b ? __a : __b; })
-#define GMAX(A,B)	({ __typeof__(A) __a = (A); __typeof__(B) __b = (B); __a < __b ? __b : __a; })
-#define GFloatEqual(A,B)({GFloat d = (GFloat)A - (GFloat)B;d > -0.00001 && d < 0.00001;})
-#if defined( __cplusplus )
-#   define DEFAULT_PARAM(x) =x
+#if defined(__cplusplus)
+#define DEFAULT_PARAM(x) = x
 #else
-#   define DEFAULT_PARAM(x)
+#define DEFAULT_PARAM(x)
 #endif
 
-#define  GALIGN(A,B) ((A) & ~(B-1))
+#define GALIGN(A, B) ((A) & ~(B - 1))
 
 #endif /* GJPlatformHeader_h */
