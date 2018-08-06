731071494d08798f1dded4e5c0b8b617bd421cad
MirrorUncle <1065517719@qq.com>
2018-08-06 17:35:43 +0800

    分离引擎

--- GJLiveEngine/GJLiveEngine/CrossPlatform/PlatformBridge/Version.h
+++
@@ -1,5 +0,0 @@
-#ifndef Version_h
-#define Version_h
-static char GJLiveEngineVersion[] = "1.07.18";
-static char GJLiveEngineBuild[] = "17.48";
-#endif /* Version_h */
--- GJLiveEngine/GJLiveEngine/CrossPlatform/Tools/GJFLVPack.c
+++
@@ -1,9 +0,0 @@
-//
-//  GJFLVPack.c
-//  GJCaptureTool
-//
-//  Created by melot on 2017/6/13.
-//  Copyright © 2017年 MinorUncle. All rights reserved.
-//
-
-#include "GJFLVPack.h"
